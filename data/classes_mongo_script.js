ed63e76f296ff9e2595405a11e57928e66e8c885
Daniel Deutsch <danieldeutsch@gmail.com>
2013-04-16 07:39:00 -0400

    added script for adding classes

---
+++ data/classes_mongo_script.js
@@ -0,0 +1,7 @@
+//Example of how to load file in Mongo shell:
+//load("/Users/danieldeutsch/Documents/Junior\ Year/Spring/15237/studyshare/data/classes_mongo_script.js");
+
+db.classes.insert({name : "Cross-Platform Mobile Web Apps", num : 15237, deptNum : 15, classNum : 237});
+db.classes.insert({name : "Distributed Systems", num : 15440, deptNum : 15, classNum : 440});
+db.classes.insert({name : "Set Theory", num : 21329, deptNum : 21, classNum : 329});
+db.classes.insert({name : "Graph Theory", num: 21484, deptNum : 21, classNum : 484});
